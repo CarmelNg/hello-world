8787a3f7569b40224638ffeb98ee9fd884f85182
Carmel  Nguelebek <97544914+CarmelNg@users.noreply.github.com>
2022-12-14 00:33:55 -0500

    change

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-/*********************************************************************************
+/********************************************************************************
 *  WEB322 – Assignment 06
-*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part of this
-*  assignment has been copied manually or electronically from any other source (including web sites) or 
-*  distributed to other students.
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
 * 
-*  Name: Amitoj Singh    Student ID: 159347210    Date: 02/12/2022 (dd/mm/yyyy)
+*  Name: _Carmel Cheumadjeu Nguelebek__ Student ID: _130451214_ Date: _12/12//2022____
 *
-*  Online (Cyclic) Link: https://long-teal-lemming-cape.cyclic.app/a
+*  Online (Heroku) Link: https://burgundy-bull-sock.cyclic.app
 *
-********************************************************************************/ 
+********************************************************************************/
 
 
 const express = require('express');
